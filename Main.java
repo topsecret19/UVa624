ac61f5fe84f66b48b71378a07e0bac64996318d9
topsecret19 <39679835+topsecret19@users.noreply.github.com>
2018-08-05 11:38:02 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,19 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 public class Main {
+	static HashMap<String,Boolean> answers = new HashMap<String,Boolean>();
 	static HashMap<Integer,Integer> real = new HashMap<Integer,Integer>();
-	public static boolean tracks(int index,HashMap<Integer,Integer> m,int sum,int target) {
+	static int target;
+	static HashMap<Integer,Integer> m = new HashMap<Integer,Integer>();
+	public static boolean tracks(int index,int sum) {
+		String answer = index+"-"+sum;
 		if(sum==target) {
 			real.put(index, m.get(index));
+			answers.put(answer, true);
 			return true;
 		}
-		if(index==m.size()) return false;
-		if(tracks(index+1,m,sum+m.get(index),target)) {
+		if(answers.get(answer)!=null) return answers.get(answer);
+		if(index==m.size()) {
+			answers.put(answer, false);
+			return false;
+		}
+		if(tracks(index+1,sum+m.get(index))) {
 			real.put(index, m.get(index));
+			answers.put(answer, true);
 			return true;
 		}
-		
-		return tracks(index+1,m,sum,target);
+		return tracks(index+1,sum);
 	}
 	
 public static void main(String[] args) throws IOException {
@@ -23,13 +32,12 @@ public static void main(String[] args) throws IOException {
 	String line;
 	while((line=br.readLine())!=null&&!line.isEmpty()) {
 		Scanner sc = new Scanner(line);
-		int target = sc.nextInt();
+		target = sc.nextInt();
 		int n= sc.nextInt();
-		HashMap<Integer,Integer> m = new HashMap<Integer,Integer>();
 		m.clear();
 		for(int i=0; i<n; i++) m.put(i, sc.nextInt());
-		while(tracks(0,m,0,target)!=true) target--;
-		tracks(0,m,0,target);
+		while(tracks(0,0)!=true) target--;
+		tracks(0,0);
 		int sum2 =0;
 		for(int i=0; i<n; i++) {
 			if(real.get(i)!=null) {
@@ -44,6 +52,7 @@ public static void main(String[] args) throws IOException {
 		}
 		System.out.println("sum:"+sum2);
 		real.clear();
+		answers.clear();
 	}
 }
 }
